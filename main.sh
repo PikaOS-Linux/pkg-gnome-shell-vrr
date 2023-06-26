60f6b45be3ed7f36441015e1b63d8ab88d084f7c
Ward Nakchbandi (Cosmic Fusion) <hotrod.master@hotmail.com>
2023-06-27 00:17:01 +0300

    initial push

--- main.sh
+++ main.sh
@@ -1,8 +1,7 @@
 DEBIAN_FRONTEND=noninteractive
 
 # Clone Upstream
-git clone https://gitlab.gnome.org/GNOME/gnome-shell -b 43.1
-mv ./gnome-shell ./gnome-shell-vrr
+git clone https://gitlab.gnome.org/GNOME/gnome-shell -b 44.2 gnome-shell-vrr
 cp -rvf ./debian ./gnome-shell-vrr
 cd ./gnome-shell-vrr
 
@@ -11,7 +10,7 @@ apt-get build-dep ./ -y
 
 # Build package
 LOGNAME=root dh_make --createorig -y -l -p gnome-shell-vrr_43.1
-dpkg-buildpackage
+dpkg-buildpackage --no-sign
 
 # Move the debs to output
 cd ../
